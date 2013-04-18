041f292c44d3d255920a3351824d4671d7dc4d8f
Yang Ling <yang_l@worksap.co.jp>
2013-04-18 12:20:01 +0800

    Add init.sh
    Automatically start ibus, vrome, wallch when boot.

--- init.sh
+++ init.sh
@@ -1,2 +1,10 @@
 #! /bin/sh
-ln -s .vimrc ~/.vimrc
+pgrep wallch >/dev/null
+result=$?
+if [ "$result" = "0" ]; then
+    # wallch already exists
+    return 0
+fi
+ibus-daemon -d -r
+nohup vrome >/dev/null 2>&1 &
+nohup wallch --constant >/dev/null 2>&1 &
