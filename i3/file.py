9a862517463e6a8904bae2fd3453ff588e484653
Yang Ling <yangling1984@gmail.com>
2015-07-22 19:06:13 +0800

    Improve file.py

--- i3/file.py
+++ i3/file.py
@@ -11,21 +11,6 @@ status = Status(standalone=True)
 status.register("clock",
     format="%a %F %R",)
 
-# Shows the average load of the last minute and the last 5 minutes
-# (the default value for format is used)
-status.register("load")
-
-# Shows your CPU temperature, if you have a Intel CPU
-status.register("temp",
-    format="{temp:.0f}°C",)
-
-status.register("cpu_usage",)
-
-status.register("mem",
-    format="{avail_mem}GB",
-    divisor=1073741824,
-    )
-
 # The battery monitor has many formatting options, see README for details
 
 # This would look like this, when discharging (or charging)
@@ -37,32 +22,39 @@ status.register("mem",
 # goes below 5 percent while discharging. The block will also color RED.
 # If you don't have a desktop notification demon yet, take a look at dunst:
 #   http://www.knopwob.org/dunst/
-#status.register("battery",
-    #format="{status}/{consumption:.2f}W {percentage:.2f}% [{percentage_design:.2f}%] {remaining:%E%hh:%Mm}",
-    #alert=True,
-    #alert_percentage=5,
-    #status={
-        #"DIS": "↓",
-        #"CHR": "↑",
-        #"FULL": "=",
-    #},)
+status.register("battery",
+    format="{status}/{consumption:.2f}W {percentage:.2f}% [{percentage_design:.2f}%] {remaining:%E%hh:%Mm}",
+    alert=True,
+    alert_percentage=5,
+    status={
+        "DIS": "↓",
+        "CHR": "↑",
+        "FULL": "=",
+    },
+    not_present_text = "",
+    )
 
 # This would look like this:
 # Discharging 6h:51m
-#status.register("battery",
-    #format="{status} {remaining:%E%hh:%Mm}",
-    #alert=True,
-    #alert_percentage=5,
-    #status={
-        #"DIS":  "Discharging",
-        #"CHR":  "Charging",
-        #"FULL": "Bat full",
-    #},)
+status.register("battery",
+    format="{status} {remaining:%E%hh:%Mm}",
+    alert=True,
+    alert_percentage=5,
+    status={
+        "DIS":  "Discharging",
+        "CHR":  "Charging",
+        "FULL": "Bat full",
+    },
+    not_present_text = "",
+    )
 
-# Displays whether a DHCP client is running
 status.register("runwatch",
-    name="DHCP",
-    path="/var/run/dhclient*.pid",)
+    name="SSHD",
+    color_up = "#FF0000",
+    format_down = "",
+    hints = {"separator": False},
+    path="/var/run/sshd.pid",
+    )
 
 status.register("keyboard_locks",
     caps_off="",
@@ -80,7 +72,7 @@ status.register("keyboard_locks",
 status.register("network",
     interface="eno1",
     hints = {"markup": "pango"},
-    format_up = "<span color=\"#00FF00\">{essid}</span> {bytes_recv:6.1f}KiB {bytes_sent:5.1f}KiB",
+    format_up = "<span color=\"#00FF00\">{v4}</span> {bytes_recv:6.1f}KiB {bytes_sent:5.1f}KiB",
     format_down = "",
     dynamic_color = True,
     start_color = "#00FF00",
@@ -94,31 +86,36 @@ status.register("network",
     #interface="wlan0",
     #format_up="{essid} {quality:03.0f}%",)
 
-# Shows disk usage of /
-# Format:
-# 42/128G [86G]
-#status.register("disk",
-    #path="/",
-    #format="{used}/{total}G [{avail}G]",)
+# Shows the average load of the last minute and the last 5 minutes
+# (the default value for format is used)
+status.register("load")
 
-# Shows pulseaudio default sink volume
-#
-# Note: requires libpulseaudio from PyPI
+# Shows your CPU temperature, if you have a Intel CPU
+status.register("temp",
+    format="{temp:.0f}°C",)
+status.register("cpu_usage_graph",)
+status.register("cpu_usage",
+    # fa-laptop [&#xf109;]
+    format = "{usage:02}%",
+    hints = {"separator": False},
+    )
+status.register("mem_swap",
+    # fa-file-o [&#xf016;]
+    format="{used_swap}GB",
+    divisor=1073741824,
+    )
+status.register("mem_bar",)
+status.register("mem",
+    # fa-flash (alias) [&#xf0e7;]
+    format="{avail_mem}GB",
+    hints = {"separator": False},
+    divisor=1073741824,
+    )
 status.register("alsa",
-    format="♪{volume}",
+    # fa-volume-up [&#xf028;]
+    format="{volume}",
     mixer="PCM",
     card=1,
     )
-
-# Shows mpd status
-# Format:
-# Cloud connected▶Reroute to Remain
-#status.register("mpd",
-    #format="{title}{status}{album}",
-    #status={
-        #"pause": "▷",
-        #"play": "▶",
-        #"stop": "◾",
-    #},)
 
 status.run()
