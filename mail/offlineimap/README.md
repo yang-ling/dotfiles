c274c453e7abb09bf22dce0fa6236ad6a8c5ce6a
Yang Ling <yangling1984@gmail.com>
2015-12-18 19:49:12 +0800

    Add offlineimaphook.

--- mail/offlineimap/README.md
+++ mail/offlineimap/README.md
@@ -1,3 +1,3 @@
 ## Offlineimap
 
-Put `offlineimap.conf` and `offlineimap.py` to `~/.config/offlineimap/` and use `offlineimap -c ~/.config/offlineimap/offlineimap.conf` to specify config file.
+Put `offlineimap.conf` `offlineimaphook.sh` and `offlineimap.py` to `~/.config/offlineimap/` and use `offlineimap -c ~/.config/offlineimap/offlineimap.conf` to specify config file.
---
+++ mail/offlineimap/offlineimaphook.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+MAIL_ROOT="$HOME/Mails"
+message=""
+
+find "$MAIL_ROOT" -type d -path "$MAIL_ROOT/.notmuch" -prune -o -type d -print0 | while read -r -d $'\0' oneFolder
+do
+    NEW="new"
+    if  test "x${oneFolder##*/}" = "x$NEW" ; then
+        newmail_count=$(find "$oneFolder" -type f | wc -l)
+        mailbox="${oneFolder%/new}"
+        account="${mailbox%/*}"
+        mailbox="${mailbox##*/}"
+        account="${account##*/}"
+        [[ $newmail_count -gt 0 ]] && \
+            notify-send "$newmail_count New Mail(s) in $account/$mailbox " "$message" -t 5000 &
+    fi
+done
+
+notmuch new &
