050ff4f50c3278e33942f738bb9b34489a585351
Yang Ling <yangling1984@gmail.com>
2015-12-18 19:04:54 +0800

    Add offlineimap config.

--- mail/offlineimap/offlineimap.py
+++ mail/offlineimap/offlineimap.py
@@ -5,20 +5,49 @@ from subprocess import check_output
 
 netease_names = {"&dcVr0mWHTvZZOQ-": "病毒文件夹",
                  "&g0l6P3ux-": "草稿箱",
-                 "&g0l6Pw-": "unknown1",
+                 "&g0l6Pw-": "草稿",
                  "&i6KWBZCuTvY-": "订阅邮件",
                  "&V4NXPpCuTvY-": "垃圾邮件",
                  "&XfJSIJZk-": "已删除",
                  "&XfJT0ZAB-": "已发送",
-                 "&XfJT0ZABkK5O9g-": "unknown2",
+                 "&XfJT0ZABkK5O9g-": "unknown",
                  "&Xn9USpCuTvY-": "广告邮件"
                  }
-
-
-def trans_yanglingit163(foldername):
-    return netease_names.get(foldername, foldername)
+gmail_names = {
+    "[Gmail]/&XfJSoGYfaAc-": "已加星标",
+    "[Gmail]/&XfJT0ZCuTvY-": "已发邮件",
+    "[Gmail]/&g0l6Pw-": "草稿",
+    "[Gmail]/&V4NXPpCuTvY-": "垃圾邮件",
+    "[Gmail]/&YkBnCZCuTvY-": "所有邮件",
+    "[Gmail]/&XfJSIJZkkK5O9g-": "已删除邮件",
+    "[Gmail]/&kc2JgQ-": "Important",
+    "&ZcWITHb4UXM-": "旅行相关",
+    "&ZTZjbg-": "收据",
+}
 
 
 def get_pass(filename):
     command_content = "gpg -dq ~/.config/offlineimap/{0}.asc".format(filename)
     return check_output(command_content, shell=True).strip("\n")
+
+
+def trans_yanglingit163_remote(foldername):
+    return netease_names.get(foldername, foldername)
+
+
+def trans_yanglingit163_local(foldername):
+    for k, v in netease_names.iteritems():
+        if v == foldername:
+            return k
+    return foldername
+
+
+def trans_yangling1984gmail_remote(foldername):
+    return gmail_names.get(foldername, foldername)
+
+
+def trans_yangling1984gmail_local(foldername):
+    for k, v in gmail_names.iteritems():
+        if v == foldername:
+            return k
+    return foldername
