e210988e66e91bcb150b6f34e278a3e14bfadad0
Yang Ling <yangling1984@gmail.com>
2016-03-27 23:19:57 +0800

    Lower critical_limit of home disk.

---
+++ i3/file-bottom-home.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from i3pystatus import Status
+from i3pystatus.mail import thunderbird
+
+status = Status(standalone=True)
+
+# Displays clock like this:
+# Tue 30 Jul 11:59:46 PM KW31
+#                          ^-- calendar week
+status.register("disk",
+    path="/",
+    critical_limit = 5,
+    format = "root: {avail}GB/{total}GB",
+    )
+status.register("disk",
+    path="/boot",
+    critical_limit = 20,
+    divisor = 1024 ** 2,
+    format = "boot: {avail}MB/{total}MB",
+    )
+status.register("disk",
+    path="/home",
+    critical_limit = 20,
+    format = "home: {avail}GB/{total}GB",
+    )
+status.register("usb",
+    hints = {"markup": "pango"},
+    )
+status.register("mpd",
+    format = "{album} - {artist} - {title} {status}",
+    )
+#client_gmail=thunderbird.Thunderbird(account="yangling1984@gmail.com")
+#client_163_1=thunderbird.Thunderbird(account="yuloo911@163.com")
+#client_163_2=thunderbird.Thunderbird(account="yanglingit@163.com")
+#client_outlook=thunderbird.Thunderbird(account="yanglingit@outlook.com")
+#status.register("mail",
+        #backends=[client_gmail, client_163_1, client_163_2, client_outlook],
+        #hide_if_null=True,
+        #email_client="thunderbird",
+    #)
+
+status.run()
