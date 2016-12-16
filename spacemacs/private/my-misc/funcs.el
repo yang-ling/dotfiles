0306f64e67907daf39b05fcfc19e984dc3ec4ef8
Yang Ling <yangling1984@gmail.com>
2016-12-16 19:28:04 +0800

    Format

--- spacemacs/private/my-misc/funcs.el
+++ spacemacs/private/my-misc/funcs.el
@@ -8,41 +8,41 @@
 ;; }}}
 
 (defun create-temp-file ()
-    "Create a temp file"
-    (interactive)
-    (make-temp-file "emacs"))
+  "Create a temp file"
+  (interactive)
+  (make-temp-file "emacs"))
 
 (defun mu-xml-format ()
-    "Format an XML buffer with `xmllint'."
-    (interactive)
-    (shell-command-on-region (point-min) (point-max)
-        "xmllint -format -"
-        (current-buffer)
-        t
-        "*Xmllint Error Buffer*"
-        t))
+  "Format an XML buffer with `xmllint'."
+  (interactive)
+  (shell-command-on-region (point-min) (point-max)
+                           "xmllint -format -"
+                           (current-buffer)
+                           t
+                           "*Xmllint Error Buffer*"
+                           t))
 (defun my-copy-file-path (choice)
   "Copy the current buffer's file path or dired path to `kill-ring'.
 Result is full path. "
   (interactive "c(f)ull file path or (d)irectory path or just file (n)ame:")
   (let ((-fpath
-          (if (equal major-mode 'dired-mode)
-            (expand-file-name default-directory)
-            (if (null (buffer-file-name))
-              (user-error "Current buffer is not associated with a file.")
-              (buffer-file-name)))))
+         (if (equal major-mode 'dired-mode)
+             (expand-file-name default-directory)
+           (if (null (buffer-file-name))
+               (user-error "Current buffer is not associated with a file.")
+             (buffer-file-name)))))
     (kill-new
-      (cond
-        ((char-equal ?f choice) (progn
-                                  (message "File path copied: 「%s」" -fpath)
-                                  -fpath))
-        ((char-equal ?d choice) (progn
-                                  (message "Directory path copied: 「%s」" (file-name-directory -fpath))
-                                  (file-name-directory -fpath)))
-        ((char-equal ?n choice) (progn
-                                  (message "File name copied: 「%s」" (file-name-nondirectory -fpath))
-                                  (file-name-nondirectory -fpath)))
-        (t (error "Invalid input"))))))
+     (cond
+      ((char-equal ?f choice) (progn
+                                (message "File path copied: 「%s」" -fpath)
+                                -fpath))
+      ((char-equal ?d choice) (progn
+                                (message "Directory path copied: 「%s」" (file-name-directory -fpath))
+                                (file-name-directory -fpath)))
+      ((char-equal ?n choice) (progn
+                                (message "File name copied: 「%s」" (file-name-nondirectory -fpath))
+                                (file-name-nondirectory -fpath)))
+      (t (error "Invalid input"))))))
 
 (defun load-if-exists (f)
   (let ((filename (expand-file-name f)))
--- spacemacs/private/my-notmuch/config.el
+++ spacemacs/private/my-notmuch/config.el
@@ -1,26 +1,25 @@
 ;; Settings to work with msmtp
 (setq mail-specify-envelope-from t
-  send-mail-function 'sendmail-send-it
-  sendmail-program "/usr/local/bin/my-send-mail.sh"
-  mail-envelope-from 'header
-  message-sendmail-f-is-evil nil
-  message-sendmail-envelope-from 'header)
+      send-mail-function 'sendmail-send-it
+      sendmail-program "/usr/local/bin/my-send-mail.sh"
+      mail-envelope-from 'header
+      message-sendmail-f-is-evil nil
+      message-sendmail-envelope-from 'header)
 
 ;; Automatically check signatures
 (setq notmuch-crypto-process-mime t)
 (setq notmuch-draft-tags '("+draft" "-inbox" "-unread")
-  notmuch-archive-tags '("-inbox" "+archive"))
+      notmuch-archive-tags '("-inbox" "+archive"))
 
 ;; Crypto Settings
 ;; (add-hook 'message-setup-hook 'mml-secure-sign-pgpmime)
 ;; (setq epg-gpg-program "/usr/bin/gpg2")
-
 (add-hook 'notmuch-hello-refresh-hook
-  (lambda ()
-    (if (and (eq (point) (point-min))
-          (search-forward "Saved searches:" nil t))
-      (progn
-        (forward-line)
-        (widget-forward 1))
-      (if (eq (widget-type (widget-at)) 'editable-field)
-        (beginning-of-line)))))
+          (lambda ()
+              (if (and (eq (point) (point-min))
+                       (search-forward "Saved searches:" nil t))
+                  (progn
+                    (forward-line)
+                    (widget-forward 1))
+                (if (eq (widget-type (widget-at)) 'editable-field)
+                    (beginning-of-line)))))
