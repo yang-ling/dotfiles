20f8c0fa7df029ea1230ed659cb43577596e9a37
Yang Ling <yangling1984@gmail.com>
2016-12-21 17:30:10 +0800

    Add org-html-validation-link setting.

--- spacemacs/common-config.el
+++ spacemacs/common-config.el
@@ -4,7 +4,8 @@
       evil-escape-delay 0.5
       evil-escape-key-sequence "fd"
       evil-search-wrap nil
-      scroll-margin 5)
+      scroll-margin 5
+      org-html-validation-link nil)
 
 (with-eval-after-load "tramp"
   (setenv "SHELL" "/bin/bash")
