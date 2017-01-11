546794aaa902605b342887dfd41a36e4cff6a52c
Yang Ling <yangling1984@gmail.com>
2017-01-11 20:49:34 +0800

    Set org capture config

--- spacemacs/private/my-misc/keybindings.el
+++ spacemacs/private/my-misc/keybindings.el
@@ -3,6 +3,7 @@
 
 (spacemacs/set-leader-keys-for-major-mode 'nxml-mode "ox" 'mu-xml-format)
 (spacemacs/set-leader-keys-for-major-mode 'org-mode
+  "oc" 'org-columns
   "oo" 'outline-show-all
   "oO" 'outline-hide-other
   "oh" 'outline-hide-body
